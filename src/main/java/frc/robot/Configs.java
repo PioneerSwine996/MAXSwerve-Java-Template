2e30de5348c8fe2b18fbccf206526f669769418e
PioneerSwine996 <129558257+PioneerSwine996@users.noreply.github.com>
2025-03-01 20:24:38 -0800

    Changed limit

--- src/main/java/frc/robot/Configs.java
+++ src/main/java/frc/robot/Configs.java
@@ -20,7 +20,7 @@ public final class Configs {
 
             drivingConfig
                     .idleMode(IdleMode.kBrake)
-                    .smartCurrentLimit(50);
+                    .smartCurrentLimit(35);
             drivingConfig.encoder
                     .positionConversionFactor(drivingFactor) // meters
                     .velocityConversionFactor(drivingFactor / 60.0); // meters per second
