5b70dc4f1f0b3647971b536929302b2f28a434c7
PioneerSwine996 <129558257+PioneerSwine996@users.noreply.github.com>
2025-03-20 21:23:29 -0700

    Swerve

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -17,8 +17,6 @@ import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj.PS4Controller.Button;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
@@ -28,103 +26,76 @@ import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.OIConstants;
 import frc.robot.subsystems.DriveSubsystem;
 
+
+/*
+ * This class is where the bulk of the robot should be declared.  Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls).  Instead, the structure of the robot
+ * (including subsystems, commands, and button mappings) should be declared here.
+ */
 public class RobotContainer {
-  // --- Subsystems from the first code ---
-  private final DriveSubsystem swerveDriveSubsystem = new DriveSubsystem();
+  // The robot's subsystems
+  private final DriveSubsystem m_robotDrive = new DriveSubsystem();
 
-  // --- Subsystems from the second code ---
-//   private final CANRollerSubsystem rollerSubsystem = new CANRollerSubsystem();
-//   private final ElevatorSubSystems elevatorSubSystems = new ElevatorSubSystems();
-//   private final PivotSubSystem pivotSubSystem = new PivotSubSystem();
-//   private final AlgaeSubSystem algaeSubSystem = new AlgaeSubSystem();
-//   private final AlageRollerSubsystem alageRollerSubsystem = new AlageRollerSubsystem();
-//   private final AutoRoller autoRoller = new AutoRoller(rollerSubsystem, 0);
-
-  // --- Controllers ---
-  // Swerve drive uses a traditional XboxController.
-  private final XboxController swerveDriverController = new XboxController(OIConstants.kDriverControllerPort);
-  
-  // The second code uses CommandXboxControllers.
-//   private final CommandXboxController driverCommandXboxController = new CommandXboxController(OperatorConstants.DRIVER_CONTROLLER_PORT);
-  // private final CommandXboxController operatorController = new CommandXboxController(OperatorConstants.OPERATOR_CONTROLLER_PORT);
-
-  // --- Autonomous chooser ---
-  private final SendableChooser<Command> autoChooser = new SendableChooser<>();
+  // The driver's controller
+  XboxController m_driverController = new XboxController(OIConstants.kDriverControllerPort);
 
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
    */
   public RobotContainer() {
-    // Configure button bindings (merging both files’ bindings)
-    configureBindings();
-    
-    // Configure default commands for subsystems
-    configureDefaultCommands();
-    
-    // Configure autonomous options
-    configureAutoChooser();
-    SmartDashboard.putData(autoChooser);
+    // Configure the button bindings
+    configureButtonBindings();
+
+    // Configure default commands
+    m_robotDrive.setDefaultCommand(
+        // The left stick controls translation of the robot.
+        // Turning is controlled by the X axis of the right stick.
+        new RunCommand(
+            () -> m_robotDrive.drive(
+                MathUtil.applyDeadband(m_driverController.getLeftY(), OIConstants.kDriveDeadband),
+                MathUtil.applyDeadband(m_driverController.getLeftX(), OIConstants.kDriveDeadband),
+                -MathUtil.applyDeadband(m_driverController.getRightX(), OIConstants.kDriveDeadband),
+                false),
+            m_robotDrive));
   }
 
   /**
-   * Combine button bindings from both original files.
+   * Use this method to define your button->command mappings. Buttons can be
+   * created by
+   * instantiating a {@link edu.wpi.first.wpilibj.GenericHID} or one of its
+   * subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then calling
+   * passing it to a
+   * {@link JoystickButton}.
    */
-  private void configureBindings() {
-    // --- Bindings from the first code (Swerve drive) ---
-    new JoystickButton(swerveDriverController, Button.kR1.value)
-        .whileTrue(new RunCommand(() -> swerveDriveSubsystem.setX(), swerveDriveSubsystem));
-
-    // --- Bindings from the second code ---
-    // operatorController.a().whileTrue(new AutoRoller(rollerSubsystem, Constants.RollerConstants.ROLLER_EJECT_VALUE));
-    // operatorController.b().whileTrue(new AutoRoller(rollerSubsystem, Constants.RollerConstants.ROLLER_SHOOT_VALUE));
-
-    // operatorController.leftTrigger().toggleOnTrue(new Pivot(pivotSubSystem, Constants.PivotConstants.intakePosition));
-    
-    // operatorController.y().toggleOnTrue(Commands.parallel(
-    //         new AlgaePivot(algaeSubSystem, Constants.AlgaeConstants.encoderSetpoint),
-    //         new AlageRoller(alageRollerSubsystem, Constants.AlageRollerConstants.ALAGE_ROLLER_INTAKE)));
-    
-    // operatorController.x().whileTrue(new AlageRoller(alageRollerSubsystem, Constants.AlageRollerConstants.AlAGE_ROLLER_SHOOT));
-    
-    // (Optional: if left bumper binding is needed, it can be added here)
-    // operatorController.leftBumper().whileTrue(Commands.parallel());
-
-    // operatorController.rightTrigger().toggleOnTrue(new ElevatorPID(elevatorSubSystems, Constants.ElevatorConstants.encoderSetpoint));
-    // operatorController.rightBumper().toggleOnTrue(new ElevatorPID(elevatorSubSystems, Constants.ElevatorConstants.halfEncoderSetpoint));
+  private void configureButtonBindings() {
+    new JoystickButton(m_driverController, Button.kR1.value)
+        .whileTrue(new RunCommand(
+            () -> m_robotDrive.setX(),
+            m_robotDrive));
   }
 
   /**
-   * Set default commands for all subsystems.
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
    */
-  private void configureDefaultCommands() {
-    // --- Default command for the swerve drive subsystem (first code) ---
-    swerveDriveSubsystem.setDefaultCommand(
-        new RunCommand(() ->
-            swerveDriveSubsystem.drive(
-                -MathUtil.applyDeadband(swerveDriverController.getLeftY(), OIConstants.kDriveDeadband),
-                -MathUtil.applyDeadband(swerveDriverController.getLeftX(), OIConstants.kDriveDeadband),
-                -MathUtil.applyDeadband(swerveDriverController.getRightX(), OIConstants.kDriveDeadband),
-                true),
-            swerveDriveSubsystem));
-
-  }
-
-  /**
-   * Configure autonomous command options by merging the autonomous routines.
-   */
-  private void configureAutoChooser() {
-    // --- Option 1: Swerve Trajectory Autonomous from the first file ---
+  public Command getAutonomousCommand() {
+    // Create config for trajectory
     TrajectoryConfig config = new TrajectoryConfig(
         AutoConstants.kMaxSpeedMetersPerSecond,
         AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+        // Add kinematics to ensure max speed is actually obeyed
         .setKinematics(DriveConstants.kDriveKinematics);
 
+    // An example trajectory to follow. All units in meters.
     Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(
-        // Start at the origin facing the +X direction.
+        // Start at the origin facing the +X direction
         new Pose2d(0, 0, new Rotation2d(0)),
-        // Interior waypoints.
+        // Pass through these two interior waypoints, making an 's' curve path
         List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
-        // End pose.
+        // End 3 meters straight ahead of where we started, facing forward
         new Pose2d(3, 0, new Rotation2d(0)),
         config);
 
@@ -134,55 +105,20 @@ public class RobotContainer {
 
     SwerveControllerCommand swerveControllerCommand = new SwerveControllerCommand(
         exampleTrajectory,
-        swerveDriveSubsystem::getPose, // Pose supplier
+        m_robotDrive::getPose, // Functional interface to feed supplier
         DriveConstants.kDriveKinematics,
+
+        // Position controllers
         new PIDController(AutoConstants.kPXController, 0, 0),
         new PIDController(AutoConstants.kPYController, 0, 0),
         thetaController,
-        swerveDriveSubsystem::setModuleStates,
-        swerveDriveSubsystem);
+        m_robotDrive::setModuleStates,
+        m_robotDrive);
 
-    // Reset odometry for the swerve drive subsystem.
-    swerveDriveSubsystem.resetOdometry(exampleTrajectory.getInitialPose());
+    // Reset odometry to the starting pose of the trajectory.
+    m_robotDrive.resetOdometry(exampleTrajectory.getInitialPose());
 
-    autoChooser.addOption("Swerve Trajectory", 
-        swerveControllerCommand.andThen(() -> swerveDriveSubsystem.drive(0, 0, 0, false)));
-
-    // // --- Option 2: Autonomous Forward from the second file ---
-    // autoChooser.addOption("AutonomusFoward", new SequentialCommandGroup(
-    //     new AutoForawrd(canDriveSubsystem, Constants.DriveConstants.distance),
-    //     new ParallelCommandGroup(
-    //         new ElevatorPID(elevatorSubSystems, Constants.ElevatorConstants.encoderSetpoint),
-    //         new SequentialCommandGroup(
-    //             new AutoRoller(rollerSubsystem, Constants.RollerConstants.ROLLER_EJECT_VALUE)))));
-
-    // // --- Option 3: Autonomous Turn then Forward (second file) ---
-    // autoChooser.addOption("AutonomousTurnthenFoward", new SequentialCommandGroup(
-    //     new AutoTurn(canDriveSubsystem, Constants.DriveConstants.leftTurn, Constants.DriveConstants.rightTurn),
-    //     new AutoForawrd(canDriveSubsystem, Constants.DriveConstants.distance),
-    //     new ParallelCommandGroup(
-    //         new ElevatorPID(elevatorSubSystems, Constants.ElevatorConstants.encoderSetpoint)),
-    //     new SequentialCommandGroup(
-    //         new AutoRoller(rollerSubsystem, Constants.RollerConstants.ROLLER_EJECT_VALUE))));
-
-    // // --- Option 4: AprilTag Test (second file) ---
-    // autoChooser.addOption("AprilTag Test", new SequentialCommandGroup(new AprilTag(canDriveSubsystem)));
-
-    // // Set a default option (here we choose Autonomous Forward)
-    // autoChooser.setDefaultOption("AutonomusFoward", new SequentialCommandGroup(
-    //     new AutoForawrd(canDriveSubsystem, Constants.DriveConstants.distance),
-    //     new ParallelCommandGroup(
-    //         new ElevatorPID(elevatorSubSystems, Constants.ElevatorConstants.encoderSetpoint),
-    //         new SequentialCommandGroup(
-    //             new AutoRoller(rollerSubsystem, Constants.RollerConstants.ROLLER_EJECT_VALUE)))));
-  }
-
-  /**
-   * Returns the selected autonomous command.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    return autoChooser.getSelected();
+    // Run path following command, then stop at the end.
+    return swerveControllerCommand.andThen(() -> m_robotDrive.drive(0, 0, 0, false));
   }
 }
